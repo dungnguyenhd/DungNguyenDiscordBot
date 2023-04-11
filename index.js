46cbd5ac35f3c6e68e1ed3cb87ae8803711f3ab4
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-04-11 17:22:27 +0700

    tét32

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on('ready', () => {
   console.log(`${client.user.tag} you wanna play lét play. ${process.env.TOKEN}`)
+  return
 })
 
 client.on('messageCreate', async message => {
