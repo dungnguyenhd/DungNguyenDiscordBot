8c1461e43c67ca97a41e9b4910c6dd9e6e1a179d
DungNguyen <dung1506362@nuce.edu.vn>
2023-04-12 00:04:15 +0700

    tess33

--- index.js
+++ index.js
@@ -83,7 +83,6 @@ fs.readdir('./commands/', (err, files) => {
 
 client.on('ready', () => {
   console.log(`${client.user.tag} you wanna play lét play. ${process.env.TOKEN}`)
-  client.login(process.env.TOKEN)
 })
 
 client.on('messageCreate', async message => {
@@ -154,3 +153,4 @@ client.distube
 //     )
 // )
 // .on("searchDone", () => {})
+await client.login(process.env.TOKEN)
