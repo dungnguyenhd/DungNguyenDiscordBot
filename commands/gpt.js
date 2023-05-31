ca19e72feafe0f4ef8900daa381f34f60951a223
12598511 <77835584+dungnguyenhd@users.noreply.github.com>
2023-05-31 10:06:36 +0700

    update

--- commands/gpt.js
+++ commands/gpt.js
@@ -8,40 +8,26 @@ module.exports = {
     if (regex.test(string)) return message.channel.send(`Tôi là Vivy, trợ lí ảo của server Discord NGT-Esport được lập trình bởi Dũng Nguyễn.`)
     else {
       const axios = require('axios');
-      let data = JSON.stringify({
+
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 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'
+      }
+      const requestBody = {
         "model": "gpt-3.5-turbo",
-        "messages": [
-          {
-            "role": "user",
-            "content": string
-          }
-        ]
-      });
-
-      let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://openai80.p.rapidapi.com/chat/completions',
-        headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Key': '29996176f3msh3a1d6c21c502755p155bd8jsn346846a0c0a6',
-          'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
-        },
-        data: data
-      };
-
-
+        "messages": [{ "role": "user", "content": string }]
+      }
 
       await message.channel.sendTyping();
 
-      axios.request(config)
-        .then((response) => {
-          console.log(response.data);
-          response.data.choices.map((choice) => { return message.channel.send(choice.message.content); });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+      try {
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, { headers });
+        const answer = response.data.choices[0].message.content;
+        message.channel.send(answer);
+      } catch (error) {
+        console.log(error.response.data);
+        message.channel.send(error.response.data.error.message);
+      }
     }
   }
 }
